52fa330ee100e31ac6e47638b75e52303c7f5c8f
camalzade_elvin <camalzade_elvin@outlook.com>
2019-06-29 15:49:30 +0500

    mutex is okay

--- FindForbiddenWordWpf/App.xaml.cs
+++ FindForbiddenWordWpf/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +14,40 @@ namespace FindForbiddenWordWpf
     /// </summary>
     public partial class App : Application
     {
+        static bool isCreatedNew;
+        private Mutex mtx = new Mutex(false, "MyMutex", out isCreatedNew);
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            try
+            {
+                if (!isCreatedNew)
+                {
+                    MessageBox.Show("Application is already running");
+                    App.Current.Shutdown();
+                }
+                else
+                {
+                    base.OnStartup(e);
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+        }
+        protected override void OnExit(ExitEventArgs e)
+        {
+            try
+            {
+                if (!isCreatedNew)
+                    mtx.ReleaseMutex();
+                base.OnExit(e);
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
     }
 }
